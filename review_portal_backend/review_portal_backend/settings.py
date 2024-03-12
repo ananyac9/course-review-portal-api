c897c79bd2e501d4b38ee59494cb0bef2a02b032
Hari Shankar Karthik <harishankarkarthik.93@gmail.com>
2024-03-12 19:38:30 +0530

    ADDED: New users app for multi-user model. Migrations NOT done yet.

--- review_portal_backend/review_portal_backend/settings.py
+++ review_portal_backend/review_portal_backend/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users', # new app
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser' # custom user model
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
